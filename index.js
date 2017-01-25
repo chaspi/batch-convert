a3e2252ccd1297b57de9a6c0a6e52b69beaff958
Dominik Kaspar <dominik.kaspar@yahoo.de>
2017-01-26 00:54:22 +0100

    use callback, support files

--- index.js
+++ index.js
@@ -2,31 +2,56 @@
 
 var fs = require('fs');
 var gm = require('gm');
+var pa = require('path');
 
 var quality = 100;
 var format = 'jpg';
 
-exports.convert = function (inputDir, outputDir, options) {
-    if (!fs.existsSync(outputDir)) {
+function countingCallback(array, callback) {
+    var i = 0;
+    return function () {
+        if (++i == array.length) {
+            callback();
+        }
+    }
+}
+
+exports.convert = function (inputDir, outputDir, options, callback) {
+    if (fs.lstatSync(outputDir).isDirectory() && !fs.existsSync(outputDir)) {
         fs.mkdirSync(outputDir);
     }
 
-    fs.readdirSync(inputDir).forEach(function (file) {
-        if (fs.lstatSync(inputDir + '/' + file).isFile()) {
-            var fileName = file.substring(0, file.lastIndexOf('.'))
-            var fileType = file.substring(file.lastIndexOf('.') + 1)
-            if (options) {
-                options.forEach(function (option) {
-                    process(inputDir, outputDir, fileName, fileType, option);
-                })
+    if (fs.lstatSync(inputDir).isFile()) {
+        var file = pa.basename(inputDir)
+        inputDir = pa.dirname(inputDir)
+        processFile(inputDir, outputDir, options, file, callback);
+    } else {
+        var files = fs.readdirSync(inputDir);
+        var cc = countingCallback(files, callback);
+        files.forEach(function (file) {
+            if (fs.lstatSync(inputDir + '/' + file).isFile()) {
+                processFile(inputDir, outputDir, options, file, cc);
             } else {
-                process(inputDir, outputDir, fileName, fileType, option);
+                cc();
             }
-        }
-    });
+        });
+    }
 };
 
-function process(inputDir, outputDir, fileName, fileType, option) {
+function processFile(inputDir, outputDir, options, file, callback) {
+    var fileName = file.substring(0, file.lastIndexOf('.'))
+    var fileType = file.substring(file.lastIndexOf('.') + 1)
+    if (options) {
+        var cc = countingCallback(options, callback);
+        options.forEach(function (option) {
+            process(inputDir, outputDir, fileName, fileType, option, cc);
+        })
+    } else {
+        process(inputDir, outputDir, fileName, fileType, option, callback);
+    }
+}
+
+function process(inputDir, outputDir, fileName, fileType, option, callback) {
     var input = inputDir + '/' + fileName + '.' + fileType;
     var output = outputDir + '/' + fileName + (option.postfix ? option.postfix : '') + '.' + format;
     console.log('convert ' + input + ' to ' + output + '. ...');
@@ -36,5 +61,8 @@ function process(inputDir, outputDir, fileName, fileType, option) {
     }
     image.quality(quality).setFormat(format).write(output, function (err) {
         console.log('... ' + (err ? err : output) + ' successfully converted.');
+        if (callback) {
+            callback();
+        }
     });
 }
--- test/index.js
+++ test/index.js
@@ -2,17 +2,29 @@
 
 var fs = require('fs');
 var bc = require('../index.js');
+var expect = require('chai').expect;
 
 var options = [
     {
         'height': 116,
-        'postfix': '_small'
+        'postfix': '_dir'
     }
 ];
 
 describe('Covert pdf into image', function () {
-    it('Create jpg file', function (done) {
-        bc.convert("test", "test/output", options)
-        done();
+    it('Create jpg file from folder', function (done) {
+        this.timeout(10000);
+        bc.convert("test/input", "test/output", options, function () {
+            expect(fs.existsSync("test/output/test_dir.jpg")).to.be.true
+            done();
+        })
+    })
+    it('Create jpg file from file', function (done) {
+        this.timeout(10000);
+        options[0].postfix = '_file'
+        bc.convert("test/input/test.pdf", "test/output", options, function () {
+            expect(fs.existsSync("test/output/test_file.jpg")).to.be.true
+            done();
+        })
     })
 });
